3b3cac53063b6afe42ec5907a0410ef6fe8514b3
Mahesh <maheshdravid1@gmail.com>
2024-01-24 15:16:59 +0530

    Policy page added and customized some body and form color

--- form.py
+++ form.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def sysautdit_form():
     return render_template('index.html')
+
+
+@app.route("/policy", methods=['GET', 'POST'])
+def policy():
+    return render_template('policy.html')
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True)
