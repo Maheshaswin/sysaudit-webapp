8bbb34b14e36b10751f2eb579ffda682e2efdd53
Mahesh <maheshdravid1@gmail.com>
2024-01-24 19:23:38 +0530

    Added otp feature and completed the ui

---
+++ email_otp.py
@@ -0,0 +1,19 @@
+import random
+import smtplib
+
+def generateOTP(otp_size = 6):
+    final_otp = ''
+    for i in range(otp_size):
+        final_otp = final_otp + str(random.randint(0,9))
+    return final_otp
+
+def sendEmailVerificationRequest(sender="networksecurityalerts@contus.in",receiver="mahesh.s@contus.in", custom_text="Hello, Your OTP is "):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    google_app_password = "iuwapohucoirvepl"
+    server.login(sender,google_app_password)
+    cur_otp = generateOTP()
+    msg = custom_text +  cur_otp
+    server.sendmail(sender,receiver,msg)
+    server.quit()
+    return cur_otp
--- form.py
+++ form.py
@@ -1,14 +1,45 @@
-from flask import Flask, render_template
+from flask import *  
+from random import *  
+from email_otp import *
+from pymongo import MongoClient
+
+
 app = Flask(__name__)
+
+app.secret_key = 'EmailAuthenticationByMahesh2024'
+client = MongoClient('localhost', 27017)
 
 @app.route("/", methods=['GET', 'POST'])
 def sysautdit_form():
     return render_template('index.html')
 
 
-@app.route("/policy", methods=['GET', 'POST'])
+@app.route("/policy", methods=["GET"])
 def policy():
     return render_template('policy.html')
+
+@app.route('/verify',methods = ["POST"])  
+def verify():  
+    rec_email = request.form["email"]
+       
+    current_otp = sendEmailVerificationRequest(receiver=rec_email) # this function sends otp to the receiver and also returns the same otp for our session storage
+    session['current_otp'] = current_otp
+    return render_template('verify.html')  
+
+@app.route('/validate',methods=["POST"])   
+def validate():  
+    # Actual OTP which was sent to the receiver
+    current_user_otp = session['current_otp']
+    print("Current User OTP",current_user_otp)
+    
+    # OTP Entered by the User
+    user_otp = request.form['otp'] 
+    print("User OTP : ", user_otp)
+     
+    if int(current_user_otp) == int(user_otp):  
+        return "<h3> Your email has been successfully verified! </h3>"  
+    else:
+        return "<h3> Oops! Email Verification Failure, OTP does not match. </h3>"  
 
 
 if __name__=="__main__":
