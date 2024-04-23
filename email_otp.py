289cbae76591d85f40fa299b1d4c3457905d0fa6
Maheshaswin <maheshdravid1@gmail.com>
2024-04-23 16:33:05 +0530

    Updated Appkey and mail id

--- email_otp.py
+++ email_otp.py
@@ -7,10 +7,10 @@ def generateOTP(otp_size = 6):
         final_otp = final_otp + str(random.randint(0,9))
     return final_otp
 
-def sendEmailVerificationRequest(sender="networksecurityalerts@contus.in",receiver="mahesh.s@contus.in", custom_text="Hello, Your OTP is ", subject="Email OTP Verification"):
+def sendEmailVerificationRequest(sender="maheshdravid1@gmail.com",receiver="maheshaswin86@gmail.com", custom_text="Hello, Your OTP is ", subject="Email OTP Verification"):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    google_app_password = "iuwapohucoirvepl"
+    google_app_password = "byuhiqwdovtmnfbb"
     server.login(sender,google_app_password)
     cur_otp = generateOTP()
     msg = custom_text +  cur_otp
